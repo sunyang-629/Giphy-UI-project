31bf9fed1e596b18e39b046027b4184db6db2cf8
sunyang-629 <sunyang9876@hotmail.com>
2023-09-07 19:16:57 +1000

    01-trending initialize axios instance for gifs api

---
+++ src/apis/axiosInstance.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  //todo will set the base URL as an env variable /
+  baseURL: "https://api.giphy.com/v1/gifs",
+});
+
+//** Set an axios instance here for handling request or response in the future /
+//** eg set the bear token or handle the error response */
+axiosInstance.interceptors.request.use(
+  (request) => {
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      console.log("do something ...");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
